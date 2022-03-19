f372f5242f5019fde04d0bd2648001af1238b057
Rajesh-7381 <100822691+Rajesh-7381@users.noreply.github.com>
2022-03-20 03:56:15 +0530

    Add files via upload

---
+++ swapping.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+  ;
+    int a=4,b=5;
+    a=a+b;
+    b=a-b;
+    a=a-b;
+    printf("\n after swappina a value =%d ",a);
+    printf("\n after swapping b value=%d",b);
+    return 0;
+}
